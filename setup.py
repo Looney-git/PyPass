8250425c5100d20f480993e3796835483b8b5079
Looney <112798630+Looney-git@users.noreply.github.com>
2022-09-07 17:26:53 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
-import os
-
-os.system("sudo mkdir ~/.PyPass")
-
-os.system("sudo mv ~/gh_import/PyPass.py ~/.PyPass/")
-
 with open(os.path.expanduser('~')+"/.zprofile", 'a') as f:
 
-	f.write("\nalias pypass=\"sudo python3 ~/.PyPass/PyPass.py\"")
-
-os.system("python3 ~/.PyPass/PyPass.py")
+	f.write("\nalias pypass=\"sudo python3 ~/.pypass/pypass.py\"")
